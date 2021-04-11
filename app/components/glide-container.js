6eb94ec0f1a90470acd15f0f689d7606f5425198
Gabe R <rodriguezgab1601@gmail.com>
2021-04-12 01:35:12 +0800

    Initialise horizontal slider

---
+++ app/components/glide-container.js
@@ -0,0 +1,14 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import Glide from '@glidejs/glide';
+
+export default class GlideContainerComponent extends Component {
+  @action 
+  onLoad(){
+    const horizontalGlider = new Glide('.HorizontalGlider', {
+      type: 'slider',
+      perView: 1,
+    });
+    horizontalGlider.mount();
+  }
+}
