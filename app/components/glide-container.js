33b65189927fcfbc41acfe56744faba4043058e8
Gabe R <rodriguezgab1601@gmail.com>
2021-04-12 23:21:29 +0800

    Lint

--- app/components/glide-container.js
+++ app/components/glide-container.js
@@ -9,63 +9,60 @@ export default class GlideContainerComponent extends Component {
     const glideHorizontalGlider = new Glide('.GlideHorizontalGlider', {
       type: 'slider',
       perView: 1,
-      rewind: false
+      rewind: false,
     });
     glideHorizontalGlider.mount();
 
-    new KeenSlider(".KeenVerticalGlider-1", {
+    new KeenSlider('.KeenVerticalGlider-1', {
       created: function (instance) {
         document
           .querySelector('.glide__arrow--up')
-          .addEventListener("click", function () {
-            instance.prev()
+          .addEventListener('click', function () {
+            instance.prev();
           })
         document
           .querySelector('.glide__arrow--down')
-          .addEventListener("click", function () {
-            instance.next()
-          })
-      },
-      move: function() {
-        console.log('h;i')
-      },
-      vertical: true,
-      centered: true,
-      controls: false
-    })
-    new KeenSlider(".KeenVerticalGlider-2", {
-      created: function (instance) {
-        document
-          .querySelector('.glide__arrow--up')
-          .addEventListener("click", function () {
-            instance.prev()
-          })
-        document
-          .querySelector('.glide__arrow--down')
-          .addEventListener("click", function () {
-            instance.next()
+          .addEventListener('click', function () {
+            instance.next();
           })
       },
       vertical: true,
       centered: true,
-      controls: false
-    })
-    new KeenSlider(".KeenVerticalGlider-3", {
+      controls: false,
+    });
+    new KeenSlider('.KeenVerticalGlider-2', {
       created: function (instance) {
         document
           .querySelector('.glide__arrow--up')
-          .addEventListener("click", function () {
-            instance.prev()
+          .addEventListener('click', function () {
+            instance.prev();
+          });
+        document
+          .querySelector('.glide__arrow--down')
+          .addEventListener('click', function () {
+            instance.next();
+          });
+      },
+      vertical: true,
+      centered: true,
+      controls: false,
+    });
+    new KeenSlider('.KeenVerticalGlider-3', {
+      created: function (instance) {
+        document
+          .querySelector('.glide__arrow--up')
+          .addEventListener('click', function () {
+            instance.prev();
           })
         document
           .querySelector('.glide__arrow--down')
-          .addEventListener("click", function () {
-            instance.next()
+          .addEventListener('click', function () {
+            instance.next();
           })
       },
       vertical: true,
       centered: true,
-      controls: false
-    })
+      controls: false,
+    });
   }
 }
