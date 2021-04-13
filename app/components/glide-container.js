a375d1f2274e071d7e145865a8a21dcfb0b31534
Gabe R <rodriguezgab1601@gmail.com>
2021-04-13 23:27:52 +0800

    Add refresh on horizontal change

--- app/components/glide-container.js
+++ app/components/glide-container.js
@@ -14,12 +14,13 @@ export default class GlideContainerComponent extends Component {
     glideHorizontalGlider.mount();
     glideHorizontalGlider.on('run', function(){
       console.log(glideHorizontalGlider.index);
+      glider1.refresh();
     });
 
-    new KeenSlider('.KeenVerticalGlider-1', {
+    const glider1 = new KeenSlider('.KeenVerticalGlider-1', {
       created: function (instance) {
         document
-          .querySelector('.glide__arrow--up[data-arrow-down="0"]')
+          .querySelector('.glide__arrow--up[data-arrow-up="0"]')
           .addEventListener('click', function () {
             instance.prev();
           });
@@ -35,70 +36,68 @@ export default class GlideContainerComponent extends Component {
           if (event.keyCode === 40) { //down arrow
             instance.next();
           }
-          if(event.keyCode === 37 || event.keyCode === 39){
-            instance.moveToSlide(0, 2000);
-          }
         });
       },
       vertical: true,
       centered: true,
       controls: false,
+      resetSlide: true,
     });
-    new KeenSlider('.KeenVerticalGlider-2', {
-      created: function (instance) {
-        document
-          .querySelector('.glide__arrow--up[data-arrow-down="1"]')
-          .addEventListener('click', function () {
-            instance.prev();
-          });
-        document
-          .querySelector('.glide__arrow--down[data-arrow-down="1"]')
-          .addEventListener('click', function () {
-            instance.next();
-          });
-        window.addEventListener('keydown', function (event) {
-          if (event.keyCode === 38) { //up arrow
-            instance.prev();
-          }
-          if (event.keyCode === 40) { //down arrow
-            instance.next();
-          }
-          if(event.keyCode === 37 || event.keyCode === 39){
-            instance.moveToSlide(0, 2000);
-          }
-        });
-      },
-      vertical: true,
-      centered: true,
-      controls: false,
-    });
-    new KeenSlider('.KeenVerticalGlider-3', {
-      created: function (instance) {
-        document
-          .querySelector('.glide__arrow--up[data-arrow-down="2"]')
-          .addEventListener('click', function () {
-            instance.prev();
-          });
-        document
-          .querySelector('.glide__arrow--down[data-arrow-down="2"]')
-          .addEventListener('click', function () {
-            instance.next();
-          });
-        window.addEventListener('keydown', function (event) {
-          if (event.keyCode === 38) { //up arrow
-            instance.prev();
-          }
-          if (event.keyCode === 40) { //down arrow
-            instance.next();
-          }
-          if(event.keyCode === 37 || event.keyCode === 39){
-            instance.moveToSlide(0, 2000);
-          }
-        });
-      },
-      vertical: true,
-      centered: true,
-      controls: false,
-    });
+    // const glider2 = new KeenSlider('.KeenVerticalGlider-2', {
+    //   created: function (instance) {
+    //     document
+    //       .querySelector('.glide__arrow--up[data-arrow-up="1"]')
+    //       .addEventListener('click', function () {
+    //         instance.prev();
+    //       });
+    //     document
+    //       .querySelector('.glide__arrow--down[data-arrow-down="1"]')
+    //       .addEventListener('click', function () {
+    //         instance.next();
+    //       });
+    //     window.addEventListener('keydown', function (event) {
+    //       if (event.keyCode === 38) { //up arrow
+    //         instance.prev();
+    //       }
+    //       if (event.keyCode === 40) { //down arrow
+    //         instance.next();
+    //       }
+    //       if(event.keyCode === 37 || event.keyCode === 39){
+    //         instance.moveToSlide(0, 2000);
+    //       }
+    //     });
+    //   },
+    //   vertical: true,
+    //   centered: true,
+    //   controls: false,
+    // });
+    // const glider3 = new KeenSlider('.KeenVerticalGlider-3', {
+    //   created: function (instance) {
+    //     document
+    //       .querySelector('.glide__arrow--up[data-arrow-up="2"]')
+    //       .addEventListener('click', function () {
+    //         instance.prev();
+    //       });
+    //     document
+    //       .querySelector('.glide__arrow--down[data-arrow-down="2"]')
+    //       .addEventListener('click', function () {
+    //         instance.next();
+    //       });
+    //     window.addEventListener('keydown', function (event) {
+    //       if (event.keyCode === 38) { //up arrow
+    //         instance.prev();
+    //       }
+    //       if (event.keyCode === 40) { //down arrow
+    //         instance.next();
+    //       }
+    //       if(event.keyCode === 37 || event.keyCode === 39){
+    //         instance.moveToSlide(0, 2000);
+    //       }
+    //     });
+    //   },
+    //   vertical: true,
+    //   centered: true,
+    //   controls: false,
+    // });
   }
 }
